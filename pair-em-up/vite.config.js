cf8d0f8eeeceb6d9296b1eb8f9cefbd812c4fb27
Sergey Shishkin <shishkinsa1997@gmail.com>
2025-11-05 22:02:18 +0700

    feat: add dev dependencies

--- pair-em-up/vite.config.js
+++ pair-em-up/vite.config.js
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
-  const isDev = mode === 'development'
+  const isDev = mode === "development";
 
   return {
-    base: '/shishkinsa997-JSFE2025Q3/pair-em-up/',
+    base: "/shishkinsa997-JSFE2025Q3/pair-em-up/",
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src')
-      }
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
 
     build: {
-      outDir: 'build',
+      outDir: "build",
       emptyOutDir: true,
       sourcemap: isDev,
       minify: false,
       rollupOptions: {
         output: {
-          assetFileNames: 'assets/[name].[hash][extname]',
-          chunkFileNames: 'assets/[name].[hash].js',
-          entryFileNames: 'assets/[name].[hash].js',
+          assetFileNames: "assets/[name].[hash][extname]",
+          chunkFileNames: "assets/[name].[hash].js",
+          entryFileNames: "assets/[name].[hash].js",
         },
       },
     },
@@ -36,5 +36,5 @@ export default defineConfig(({ mode }) => {
       port: 8080,
       open: true,
     },
-  }
-})
+  };
+});
