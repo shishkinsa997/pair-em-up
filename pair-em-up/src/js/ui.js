37e9f8ac87b015d6837b3fb97cb958696cac9e69
Sergey Shishkin <shishkinsa1997@gmail.com>
2025-11-07 21:22:23 +0700

    refactor: rename vars, add hud container and counters for helpers

--- pair-em-up/src/js/ui.js
+++ pair-em-up/src/js/ui.js
@@ -1,12 +1,12 @@
-import { el } from "./util.js";
-// import { state } from "./constants.js";
+import { el } from "./utils.js";
 
 export function buildUI(root) {
-  // container
   const app = el("div", { id: "app" });
 
-  //start screen
-  const startScreen = el("main", { className: "start__screen main" });
+  // start screen
+  const startScreen = el("main", {
+    className: "start__screen main",
+  });
   const startTitle = el("h1", {
     className: "start__title",
     text: "Pair 'em Up",
@@ -17,130 +17,159 @@ export function buildUI(root) {
   });
   const github = el("a", {
     className: "start__gh",
-    attrs: { href: "https://github.com/shishkinsa997", target: "_blank" },
+    attrs: {
+      href: "https://github.com/shishkinsa997",
+      target: "_blank",
+    },
     text: "GitHub: shishkinsa997",
   });
 
   const modeSection = el("div", {
     className: "mode-section",
   });
-  const classic = el("button", {
+  const classicBtn = el("button", {
     className: "mode",
-    id: "classic",
     attrs: { type: "button" },
     text: "Classic",
   });
-  const random = el("button", {
+  const randomBtn = el("button", {
     className: "mode",
-    id: "random",
     attrs: { type: "button" },
     text: "Random",
   });
-  const chaotic = el("button", {
+  const chaoticBtn = el("button", {
     className: "mode",
-    id: "chaotic",
     attrs: { type: "button" },
     text: "Chaotic",
   });
 
-  const continueGame = el("button", {
+  const continueGameBtn = el("button", {
     className: "continue",
-    id: "continue",
     attrs: { type: "button" },
     text: "Continue",
   });
-
-  const settings = el("button", {
+  const settingsBtn = el("button", {
     className: "settings",
-    id: "settings",
     attrs: { type: "button" },
     text: "Settings",
   });
-
-  const score = el("button", {
+  const scoreBtn = el("button", {
     className: "score",
-    id: "score",
     attrs: { type: "button" },
-    text: "Score",
+    text: "Results",
   });
 
-  modeSection.append(classic, random, chaotic);
+  modeSection.append(classicBtn, randomBtn, chaoticBtn);
   startScreen.append(
     startTitle,
     startSubtutle,
     github,
     modeSection,
-    continueGame,
-    settings,
-    score,
+    continueGameBtn,
+    settingsBtn,
+    scoreBtn,
   );
 
-  //game screen
-  const gameScreen = el("main", { className: "game__screen main" });
+  // game screen
+  const gameScreen = el("main", {
+    className: "game__screen main",
+    attrs: { hidden: "" },
+  });
   const modeTitle = el("h1", {
     className: "mode__title",
     text: "Classic",
   });
 
-  const gameGrid = el("div", { className: "game__grid" });
-
+  const hud = el("div", { className: "hud" });
   const currentScore = el("p", {
     className: "current__score",
     text: "Current Score: 0",
   });
   const timer = el("p", {
+    id: "timer",
     className: "timer",
-    text: "Time: 0",
+    text: "Time: 00:00",
   });
-  const controls = el("div", { className: "controls" });
-  const restart = el("button", {
+  hud.append(currentScore, timer);
+
+  const gameGrid = el("div", { className: "game__grid" });
+
+  const controls = el("div", {
+    className: "controls",
+  });
+  const restartBtn = el("button", {
     className: "restart",
-    id: "restart",
     attrs: { type: "button" },
     text: "Restart",
   });
-  const save = el("button", {
+  const saveBtn = el("button", {
     className: "save",
-    id: "save",
     attrs: { type: "button" },
     text: "Save",
   });
+  const continueBtn = el("button", {
+    className: "continue",
+    attrs: { type: "button" },
+    text: "Continue",
+  });
+  controls.append(restartBtn, saveBtn, continueBtn);
 
-  const helpers = el("div", { className: "helpers" });
-  const hints = el("button", {
+  const helpers = el("div", {
+    className: "helpers",
+  });
+  const hintsBtn = el("button", {
     className: "hints",
-    id: "hints",
     attrs: { type: "button" },
     text: "Hints",
   });
-  const revert = el("button", {
+  const revertBtn = el("button", {
     className: "revert",
-    id: "revert",
     attrs: { type: "button" },
     text: "Revert",
   });
-  const addNumbers = el("button", {
+  const addNumbersBtn = el("button", {
     className: "add-numbers",
-    id: "add-numbers",
     attrs: { type: "button" },
     text: "Add Numbers",
   });
-  const shuffle = el("button", {
+  const addNumbersCounter = el("span", {
+    className: "btn__counter",
+    text: "10",
+  });
+  addNumbersBtn.append(addNumbersCounter);
+  const shuffleBtn = el("button", {
     className: "shuffle",
     id: "shuffle",
     attrs: { type: "button" },
     text: "Shuffle",
   });
-  const eraser = el("button", {
+  const shuffleCounter = el("span", {
+    className: "btn__counter",
+    text: "5",
+  });
+  shuffleBtn.append(shuffleCounter);
+  const eraserBtn = el("button", {
     className: "eraser",
     id: "eraser",
     attrs: { type: "button" },
     text: "Eraser",
   });
-  helpers.append(hints, revert, addNumbers, shuffle, eraser);
-  controls.append(restart, save, continueGame);
-  gameScreen.append(modeTitle, gameGrid, currentScore, timer, controls, helpers, settings);
+  const eraserCounter = el("span", {
+    className: "btn__counter",
+    text: "5",
+  });
+  eraserBtn.append(eraserCounter);
+  const hintsCounter = el("span", {
+    className: "hints__counter",
+    text: "0",
+  });
+  hintsBtn.append(hintsCounter);
+  helpers.append(hintsBtn, revertBtn, addNumbersBtn, shuffleBtn, eraserBtn);
 
-  app.appendChild(gameScreen);
+  gameScreen.append(modeTitle, hud, gameGrid, controls, helpers, settingsBtn);
+
+  app.append(startScreen, gameScreen);
   root.appendChild(app);
+
+  // ux
 }
